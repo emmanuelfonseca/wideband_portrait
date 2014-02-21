e424a2a22c6b108b1867a1949524226f68a25179
Emmanuel Fonseca <efonseca@phas.ubc.ca>
2014-02-21 00:40:51 -0800

    added PCA algorithm

--- PulsePortrait.py
+++ PulsePortrait.py
@@ -7,8 +7,8 @@ def wavelet1D(indat,wave='sym8',nlevel=5,ncycle=10,threshtype='hard'):
   """
   Compute the wavelet-denoised version of an input profile/waveform. 
 
-  Required argument: 
-    - 'indat' = input data in 1-dimensional NumPy array. 
+  Required arguments: 
+    - 'indat' = input data in 1-dimensional array. 
 
   Default arguments: 
     - 'wave' = mother wavelet (default 'sym8') 
@@ -16,15 +16,14 @@ def wavelet1D(indat,wave='sym8',nlevel=5,ncycle=10,threshtype='hard'):
     - 'ncycle' = number of circulant averages to compute (default '10') 
     - 'threshtype' = type of wavelet thresholding (default 'hard')
   """
-
   nbins = len(indat)
   data  = np.zeros(nbins)
 
-  # average shifted/denoised/re-shifted data 'ncycle' times.
+  # carry out translation-invariant wavelet denoising.
   for j in range(ncycle):
     m = j - ncycle/2 - 1
     coeffs = pywt.wavedec(np.roll(indat,m),wave,level=nlevel)
-    # get coefficient threshold value.
+    # get threshold value.
     lopt = np.median(np.fabs(coeffs[1]))/0.6745*np.sqrt(2.*np.log(nbins))
     # now do wavelet thresholding.
     for k in range(1,nlevel+1):
@@ -50,7 +49,7 @@ def wavelet2D(indat,wave='sym8',nlevel=5,ncycle=10,threshtype='hard'):
   Compute the wavelet-denoised version of a set of profiles/waveforms.
   
   Required argument:
-    - 'indat' = (nbins x nchans) NumPy data array.
+    - 'indat' = (nbins x nchans) data array.
   
   Default arguments: 
     - 'wave' = mother wavelet (default 'sym8') 
@@ -58,7 +57,6 @@ def wavelet2D(indat,wave='sym8',nlevel=5,ncycle=10,threshtype='hard'):
     - 'ncycle' = number of circulant averages to compute (default '10') 
     - 'threshtype' = type of wavelet thresholding (default 'hard')
   """
-
   nbins = len(indat[:,0])
   nchan = len(indat[0,:])
 
@@ -68,7 +66,7 @@ def wavelet2D(indat,wave='sym8',nlevel=5,ncycle=10,threshtype='hard'):
   for i in range(nchan):
     prof = indat[:,i]
     data = 0.
-    # average shifted/denoised/re-shifted data 'ncycle' times.
+    # carry out translation-invariant wavelet denoising.
     for j in range(ncycle):
       m = j - ncycle/2 - 1
       coeffs = pywt.wavedec(np.roll(prof,m),wave,level=nlevel)
@@ -91,3 +89,37 @@ def wavelet2D(indat,wave='sym8',nlevel=5,ncycle=10,threshtype='hard'):
 
   # return smoothed portrait.
   return outdat
+
+def pca(indat,ncomp=5):
+  """
+  Compute the PCA-denoised version of a set of profiles/waveforms.
+ 
+  Required argument:
+    - indat = (nbins x nchan) data array.
+
+  Default argument:
+    - ncomp = number of principal components to use (default '5')
+  """
+  
+  nmes = len(indat[0,:])
+  ndim = len(indat[:,0])
+
+  print 'PCA on data with {0} dimensions, {1} measurements:'.format(ndim,nmes)
+
+  # subtract mean from each set of measurements.
+  dmean  = np.mean(indat,axis=1)
+  matrix = (indat.T-dmean).T
+
+  # compute covariance matrix.
+  cov = np.cov(indat)
+
+  # compute eigenvalues/vectors of cov, and order them.
+  eigval, eigvec = np.linalg.eigh(cov)
+  ind = (np.argsort(eigval))[::-1]
+  eigval, eigvec = eigval[ind], eigvec[:,ind]
+ 
+  # transform to new, smoothed data using desired number of components.
+  eigvec = eigvec[:,range(ncomp)] 
+  finaldata = (np.dot(eigvec,np.dot(eigvec.T,matrix)).T+dmean).T
+  
+  return finaldata
